7708d37ae53291e6d65c4ef8a04867dc84783e6e
Vilen_Bakov <bakov.vilen@yandex.ru>
2022-02-23 22:26:00 +0500

    test: Добавлена функция удаления всех данных из факультета

--- structures/structure_array.py
+++ structures/structure_array.py
@@ -2,25 +2,29 @@ class StackArray:
     """ Модель стэк массива. """
     def __init__(self):
         """ Инициализация пустого стэк массива. """
-        self.__lst = []
+        self.__stack = []
 
     def is_empty(self) -> bool:
         """ Проверка стэк массива на пустоту. """
-        return not self.__lst
+        return not self.__stack
 
     def push(self, obj) -> None:
         """ Добавление элемента в стэк массив. """
-        self.__lst.append(obj)
+        self.__stack.append(obj)
 
     def pop(self):
         """ Возвращается элемет с вершины стэк массива если он не пуст, иначе None """
         if self.is_empty():
             return None
 
-        result = self.__lst[-1]
-        self.__lst = self.__lst[:-1]
+        result = self.__stack[-1]
+        self.__stack = self.__stack[:-1]
         return result
 
     def size(self) -> int:
         """ Возвращает количество элементав в стэк массиве """
-        return len(self.__lst)
+        return len(self.__stack)
+
+    def clear(self) -> None:
+        """ Полное очищение стэк массива. """
+        self.__stack = []
